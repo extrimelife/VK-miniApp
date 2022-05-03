9a1250266a2b535919392523ccd2475d16ae185a
Roman Khilchenko <extrimelife@hotmail.com>
2022-05-03 22:07:28 +0300

    homework 2.3 part 2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,50 +9,67 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    let profileHeaderView = ProfileHeaderView()
+    private let postModel = ModelPost.makePost()
     
-    private let newButton: UIButton = {
-        let buttonNew = UIButton()
-        buttonNew.translatesAutoresizingMaskIntoConstraints = false
-        buttonNew.backgroundColor = .blue
-        buttonNew.tintColor = .white
-        buttonNew.setTitle("New Post", for: .normal)
-        buttonNew.layer.shadowOffset = CGSize(width: 4, height: 4)
-        return buttonNew
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorInset = .zero
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
+        return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavigationController()
-        setupAutoLayout()
-        
+        setupLayout()
     }
     
-    private func setupNavigationController() {
-        let navigationBar = UINavigationBarAppearance()
-        navigationBar.backgroundColor = .white
-        self.navigationController?.navigationBar.standardAppearance =  navigationBar
-        self.navigationController?.navigationBar.scrollEdgeAppearance = navigationBar
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
-        view.addSubview(newButton)
-    }
-    
-    
-    private func setupAutoLayout() {
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        
+    private func setupLayout() {
+        view.addSubview(tableView)
         NSLayoutConstraint.activate([
-            //constrain для subview
-            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
-            // constrain для UIbutton
-            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            newButton.heightAnchor.constraint(equalToConstant: 50)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
 }
+
+// MARK: UITableViewDataSourse
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
+        cell.setupCell(postModel[indexPath.row])
+        return cell
+        
+    }
+}
+
+// MARK: UiTAbleViewDelegate
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let profileHeaderView = ProfileHeaderView()
+        profileHeaderView.backgroundColor = .systemGray6
+        return profileHeaderView
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+        }
+        return 200
+        
+    }
+}
+
+
+
+        
---
+++ Navigation/Profile/UiView + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UiView + Extension.swift
+//  Navigation
+//
+//  Created by roman Khilchenko on 03.05.2022.
+//
+
+import UIKit
+
+//Идентификатор для TableView
+
+extension UIView {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
